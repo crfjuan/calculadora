82bd0f210f04a31e4cd27684219bdf4ab468979e
JUAN DAVID <davidjjuan09@gmail.com>
2024-11-22 15:33:37 -0300

    lógica

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,49 @@ import { useState } from "react"
 import"./Style.scss"
 
 export default function App() {
-  //variável de estado
-  const [primeiroValor, setSegundoValor] = useState()
-  return (
+//VARIAVEL DE ESTADO
+  //primeiroValor é a variavel de estado
+ // setPrimeiroValor é a função atualizadora
+ const [primeiroValor, setPrimeiroValor] =useState();
+ const [segundoValor, setSegundoValor] = useState();
+ const [resultado, setResultado] = useState()
+
+ // arrow function () => {}
+   // função especial
+ const capturandoPrimeiroValor = (e) => {
+   setPrimeiroValor(Number(e.target.value))
+   console.log(e.target.value)
+ }
+
+ const capturandoSegundoValor = (e) => {
+   setSegundoValor(Number(e.target.value))
+ }
+   // função  somar
+   const soma = () => {
+    setResultado(primeiroValor + segundoValor)
+   }
+   const subtração = () => {
+    setResultado(primeiroValor - segundoValor)
+   }
+   const divisão = () => {
+    setResultado(primeiroValor / segundoValor)
+   }
+   const multiplicação = () => {
+    setResultado(primeiroValor * segundoValor)
+   }
+
+ return (
     <main>
-      <h1>Calculadora</h1>
-      <input type="number"placeholder= "insira um número" />
-      <input type="number"placeholder= "insira um número" />
+<h1>Calculadora</h1>
+      <input onChange={capturandoPrimeiroValor} type="number" placeholder="Insira um número"/>
+      <input onChange={capturandoSegundoValor} type="number" placeholder="Insira um número"/>
       <div>
-        <button>+</button>
-        <button>-</button>
-        <button>/</button>
-        <button>x</button>
+        <button onClick={soma}>+</button>
+        <button onClick={subtração}>-</button>
+        <button onClick={divisão}>/</button>
+        <button onClick={ multiplicação}>x</button>
       </div>
+    <h3>{resultado}</h3>
     </main>
   )
 }
